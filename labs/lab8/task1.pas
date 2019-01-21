cafa2b97883f7b981e7168d9f0da91e047317dca
Master <master2898thetimelord@gmail.com>
2019-01-21 18:22:18 +0200

    task 1 solved

---
+++ labs/lab8/task1.pas
@@ -0,0 +1,24 @@
+program task1;
+
+var sum, number, numberCount: integer;
+    middleNum: real;
+    inputFile: text;
+
+begin
+    assign(inputFile, 'input.txt');
+    reset(inputFile);
+
+    sum := 0; numberCount := 0;
+    while not EOF(inputFile) do begin
+        readln(inputFile, number);
+        sum := sum + number;
+        numberCount := numberCount + 1;
+    end;
+
+    close(inputFile);
+
+    middleNum := sum div numberCount;
+    
+    writeln('Sum: ', sum);
+    writeln('Middle num: ', middleNum:5:3);
+end.
